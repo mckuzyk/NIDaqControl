49c35adeaa9b3c875b20c4d342afac87284ab670
Mark C. Kuzyk <mark@Marks-MacBook-Pro.local>
2018-12-20 13:20:25 -0800

    initial commit

---
+++ DAQ.py
@@ -0,0 +1,58 @@
+import nidaqmx
+
+def configure_timed_edge_counter(duration, 
+								count_chan = "/Dev3/ctr0", 
+								time_chan = "/Dev3/Ctr1InternalOutput"):
+
+	ctr = nidaqmx.task.Task()
+	ctr.ci_channels.add_ci_count_edges_chan(count_chan)
+
+	timer = nidaqmx.task.Task()
+	timer.co_channels.add_co_pulse_chan_time(time_chan,
+		low_time = .0001,
+		high_time = duration)
+
+	ctr.triggers.pause_trigger.dig_lvl_src = time_chan
+	ctr.triggers.pause_trigger.trig_type = nidaqmx.constants.TriggerType.DIGITAL_LEVEL
+	ctr.triggers.pause_trigger.dig_lvl_when = nidaqmx.constants.Level.LOW
+
+	return ctr, timer
+
+def do_timed_edge_counter(ctr, timer):
+	ctr.start()
+	timer.start()
+	while(not timer.is_task_done()):
+		continue
+	counts = ctr.read()
+	ctr.stop()
+	timer.stop()
+	return counts
+
+def configure_gated_edge_counter(count_chan = "/Dev3/ctr0", 
+								gate_chan = "/Dev3/PFI1"):
+
+	ctr = nidaqmx.task.Task()
+	ctr.ci_channels.add_crigger.dig_lvl_src = time_chan
+
+	ctr.triggers.pause_trigger.dig_lvl_src = gate_chan
+	ctr.triggers.pause_trigger.trig_type = nidaqmx.constants.TriggerType.DIGITAL_LEVEL
+	ctr.triggers.pause_trigger.dig_lvl_when = nidaqmx.constants.Level.LOW
+
+	return ctr
+
+
+def do_gated_edge_counter(ctr):
+	ctr.start()
+# Need to add some kind of delay to ensure the counter has time to finish 
+	counts = ctr.read()
+	ctr.stop()
+	return counts
+
+def configure_ao_voltage(volt_chan, min_v = -3, max_v = 3):
+	ao = nidaqmx.task.Task()
+	ao.add_ao_voltage_chan(volt_chan, min_v, max_v)
+	return ao
+
+def set_ao_voltage(ao_task, voltage):
+	ao_task.write(voltage)
+
---
+++ README.md
@@ -0,0 +1,14 @@
+# NIDaqControl
+
+This package provides some functions that allow easy interfacing with National
+Instruments DAQ cards in the lab using Python.  The functions provided are
+wrappers for the National Instruments nidaqmx-python api.
+
+### Required packages
+
+* nidaqmx-python
+
+This package is available on pip:
+```
+pip install nidaqmx-python
+```
